1b3035e5d04bf97a4197e572b2ce0aa7cd3d5057
uasaleem <78223964+uasaleem@users.noreply.github.com>
2022-05-19 14:34:00 +0100

    Add files via upload

---
+++ js/routing.js
@@ -0,0 +1,8 @@
+<script type = "text/javascript">
+    $(document).ready(function($)
+    {
+        var e0 = $(document).find('.screen_data');
+        var s1 = $(document).find('.screen_home').html();
+        e0.html(s1)
+    }
+</script>
